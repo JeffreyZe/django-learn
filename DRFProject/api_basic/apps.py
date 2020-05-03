247f18f3010f1a6d55716476097924b3bbc77805
Wang Xuze <xuze.wang.2017@sis.smu.edu.sg>
2020-05-03 21:50:58 +0800

    formatting

--- DRFProject/api_basic/apps.py
+++ DRFProject/api_basic/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class ApiBasicConfig(AppConfig):
     name = 'api_basic'
--- DRFProject/api_basic/urls.py
+++ DRFProject/api_basic/urls.py
@@ -5,7 +5,6 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('article', ArticleViewSet, basename='article')
 
-
 urlpatterns = [
     path('viewset/', include(router.urls)),
     path('viewset/<int:pk>/', include(router.urls)),
