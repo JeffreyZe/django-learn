7e928e90ee5c35d41326e30e5b949157cb649f55
JeffreyZe <JeffreyZe@users.noreply.github.com>
2020-05-06 11:20:56 +0800

    create NewsApp and its models

--- DjangoTutorial/DjangoTutorial/settings.py
+++ DjangoTutorial/DjangoTutorial/settings.py
@@ -77,7 +77,11 @@ WSGI_APPLICATION = 'DjangoTutorial.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'djangotutorial',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
